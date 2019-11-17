295e7f134bbdbf6dfa80deca089dcd3dc5f042b7
Zhang BoYao <rZhBoYao@gmail.com>
2019-11-17 21:32:15 +0800

    replace std::string with std::string_view

--- 2019CG_Lab3_105502042/2019CG_Lab3_105502042/Object.hpp
+++ 2019CG_Lab3_105502042/2019CG_Lab3_105502042/Object.hpp
@@ -11,7 +11,7 @@ class Object {
   std::vector<Face> faces;
 
 public:
-  Object(std::string s, size_t v, size_t f) : file_name{s}, v_count{v}, f_count{f}, vs{v}, faces{f} {}
+  Object(std::string_view s, size_t v, size_t f) : file_name{s}, v_count{v}, f_count{f}, vs{v}, faces{f} {}
   auto set_vertex(std::stringstream& ss, std::ifstream& asc_file, const Matrix<4>& TM);
   auto set_face(std::stringstream& ss, std::ifstream& asc_file);
   [[nodiscard]] auto get_name() { return file_name; }
